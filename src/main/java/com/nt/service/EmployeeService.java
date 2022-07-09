e85b22b96ae09d4898aee2d73087bd3f4b7fe686
NagarajuKuruba <40588615+NagarajuKuruba@users.noreply.github.com>
2022-07-09 11:17:15 +0530

    MP-3344 [Jalandhar] Created Getall API

--- src/main/java/com/nt/service/EmployeeService.java
+++ src/main/java/com/nt/service/EmployeeService.java
@@ -1,5 +1,6 @@
 package com.nt.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,6 +25,9 @@ public class EmployeeService {
 			eee=optinal.get();
 		}
 		return eee;
+	}
+	public List<Employee> getEmployeeDetails() {
+		return emrepo.findAll();
 	}
 
 }
